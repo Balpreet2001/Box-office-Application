ec90efead9dc3b1abaaceb15db80be42ba5cccd5
Balpreet2001 <balpreetsingh8845@gmail.com>
2023-04-26 22:56:39 +0530

    added navigation using react router along with pages and navigation components

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,34 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from "./pages/Home"
+import Starred from './pages/Starred';
+import MainLayout from './components/MainLayout';
+
 function App() {
   return (
-    <div>
-      hello
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route element={<MainLayout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/starred" element={<Starred />} />
+          <Route path="/contact" element={<div>contact</div>} />
+        </Route>
+
+        {/* <Route path="/" element={<App />}>
+          <Route index element={<Home />} />
+          <Route path="teams" element={<Teams />}>
+            <Route path=":teamId" element={<Team />} />
+            <Route path="new" element={<NewTeamForm />} />
+            <Route index element={<LeagueStandings />} />
+          </Route>
+        </Route>
+        <Route element={<PageLayout />}>
+          <Route path="/privacy" element={<Privacy />} />
+          <Route path="/tos" element={<Tos />} />
+        </Route>
+        <Route path="contact-us" element={<Contact />} /> */}
+        <Route path="*" element={<div>Not Found</div>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
